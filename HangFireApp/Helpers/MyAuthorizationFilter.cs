6f8d8031f933e8f1e36819846cbe6e412f416359
Sahil Verma <me.sahilverma@hotmail.com>
2018-12-07 00:28:26 +0530

    Initial setup

---
+++ HangFireApp/Helpers/MyAuthorizationFilter.cs
@@ -0,0 +1,16 @@
+﻿using Hangfire.Dashboard;
+using Microsoft.Owin;
+
+namespace HangFireApp.Helpers
+{
+    public class MyAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            var owinContext = new OwinContext(context.GetOwinEnvironment());
+
+            // Allow all authenticated users to see the Dashboard (potentially dangerous).
+            return owinContext.Authentication.User.Identity.IsAuthenticated;
+        }
+    }
+}
---
+++ HangFireApp/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Owin;
+using Owin;
+using System;
+
+[assembly: OwinStartup(typeof(HangFireApp.Startup))]
+
+namespace HangFireApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string connectionString = System.Configuration.ConfigurationManager.AppSettings["ConnectionString"];
+            var options = new SqlServerStorageOptions
+            {
+                PrepareSchemaIfNecessary = false,
+                QueuePollInterval = TimeSpan.FromSeconds(30) // Default value
+            };
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString, options);
+            app.UseHangfireServer();
+            app.UseHangfireDashboard("/jobs");
+
+            BackgroundJob.Schedule(() => Welcome(), TimeSpan.FromMinutes(1));
+        }
+
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Wold");
+            Console.ReadKey();
+        }
+    }
+}
